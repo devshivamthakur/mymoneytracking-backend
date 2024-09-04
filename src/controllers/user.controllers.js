eb9996b241a0714b23099a8ad43820ebd8b96a8d
shivam <64252021+itsprogrammingwithme@users.noreply.github.com>
2024-09-04 21:55:54 +0530

    feat: login api

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,4 +1,6 @@
+import { UserModal } from "../models/user.modal.js";
 import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import joi from "joi"
 
@@ -7,14 +9,36 @@ const loginUser = asyncHandler(async (req, res) => {
         name: joi.string().required().min(3).max(14),
         email: joi.string().email().required(),
         googleId: joi.string().required(),
+        profileUrl: joi.string(),
+        deviceId: joi.string()
     })
 
-    const {error}  = schema.validate(req.body)
-    if(error) {
+    const { error } = schema.validate(req.body)
+    if (error) {
         throw new ApiError(400, error?.message)
     }
 
-    
+    let user = await UserModal.findOne({    
+        email: req.body.email
+    })
+
+    if (user) {
+        return res.status(200).json(new ApiResponse(200, {user, accessToken : user.generateAccessToken()}, "user logged in successfully."))
+
+    }
+
+    user = await UserModal.create({
+        email: req.body.email,
+        name: req.body.name,
+        googleId: req.body.googleId,
+        profileUrl: req.body.profileUrl || "",
+        deviceId: req.body.deviceId || "",
+    })
+
+    user.accessToken = user.generateAccessToken()
+
+    return res.status(200).json(new ApiResponse(200, user.toJSON(), "user logged in successfully"))
+
 
 })
 
--- src/models/user.modal.js
+++ src/models/user.modal.js
@@ -1,4 +1,5 @@
 import {Schema, model} from "mongoose"
+import jwt from "jsonwebtoken"
 
 const schema = new Schema ({
    name:{
@@ -21,4 +22,16 @@ const schema = new Schema ({
     timestamps:true,
 })
 
+schema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            googleId: this.googleId,
+        },
+        process.env.JWT_ACCESS_TOKEN
+    )
+}
+
 export const UserModal = model("user", schema)
+
